83b2ccd495017e29c18cfd9206d934294cfb1bfb
nick <nick@haimi.com>
2020-10-10 12:30:10 +0800

    drone

--- Dockerfile
+++ Dockerfile
@@ -7,15 +7,8 @@ RUN chmod +x /start.sh  && \
     echo "https://mirrors.aliyun.com/alpine/latest-stable/community" >> /etc/apk/repositories && \
     apk --update add --no-cache && apk add --no-cach curl
 
+ENV PLUGIN_API ""
+
+ENV PLUGIN_TOKEN ""
+
 CMD [ "/start.sh" ]
-
-# path/key:path/file,...
-ENV CONSUL_KEYFILE ""
-
-#get or put
-ENV CONSUL_OPT get
-
-# interval second
-ENV CONSUL_INTERVAL 60
-
-ENV CONSUL_HTTP_ADD http://consul:8500
--- start.sh
+++ start.sh
@@ -1,23 +1,12 @@
 #!/bin/sh
-if [ "$CONSUL_KEYFILE" = "" ];then
-    echo "CONSUL_KEYFILE is empty"
+if [ "$PLUGIN_TOKEN" = "" ];then
+    echo "PLUGIN_TOKEN is empty"
     exit 1
 fi
-while true;do
-    kf_array=${CONSUL_KEYFILE//,/ }
-    for kfa in $kf_array;do
-        key=${kfa%:*}
-        file=${kfa#*:}
-        perfix=${key:0:1}
-        if [ "$perfix" = "/" ];then
-            key=${key:1}
-        fi
-        if [ "$CONSUL_OPT" = "put" ];then
-            content=`cat ${file}`
-            curl -s -X PUT -d "${content}" "${CONSUL_HTTP_ADDR}/v1/kv/${key}"
-        else
-            curl -s -o "${file}" --create-dirs "${CONSUL_HTTP_ADDR}/v1/kv/${key}?raw=true"
-        fi
-    done
-    sleep $CONSUL_INTERVAL
-done
+if [ "$PLUGIN_API" = "" ];then
+    echo "PLUGIN_API is empty"
+    exit 1
+fi
+curl -k -X POST -s -H "Content-Type: application/json" \
+-H 'Authorization: Bearer '$PLUGIN_TOKEN \
+$PLUGIN_API'?action=redeploy'
