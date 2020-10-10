62c5ba89904ceadc91e7133a9e6feb7761208e1c
nick <nick@haimi.com>
2020-10-10 16:06:52 +0800

    test

--- start.sh
+++ start.sh
@@ -9,6 +9,9 @@ if [ "$PLUGIN_API" = "" ];then
 fi
 PLUGIN_API=${PLUGIN_API/\/p\//\/v3\/project\/}
 PLUGIN_API=${PLUGIN_API/\/workload\//\/workloads\/}
+echo $PLUGIN_API
+echo $PLUGIN_TOKEN
 curl -k -X POST -s -H "Content-Type: application/json" \
 -H 'Authorization: Bearer '$PLUGIN_TOKEN \
 $PLUGIN_API'?action=redeploy'
+echo "redeploy successfully"
